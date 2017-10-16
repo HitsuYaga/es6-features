cfab1a51f4222e27000e654a397d68f4ff7b626b
dq.tho89 <dq.tho89@gmail.com>
2017-10-16 16:32:43 +0700

    Add const feature

---
+++ features/const.js
@@ -0,0 +1,14 @@
+// const PORT = 3000;
+// console.log(PORT);
+// PORT = 3001;
+// console.log(PORT);
+// var PORT = 3000;
+// console.log(PORT);
+
+const CONFIG = {
+  PORT: 3000
+}
+
+console.log(CONFIG);
+CONFIG.PORT = 3001;
+console.log(CONFIG);
