0d2d28a6c9ec21193fde2fd9b779c4208398af1a
dq.tho89 <dq.tho89@gmail.com>
2017-10-16 16:44:57 +0700

    Add let feature

---
+++ features/let.js
@@ -0,0 +1,21 @@
+// var x = 0;
+// if (true) {
+//   var x = 12;
+//   console.log(x);
+// }
+// console.log(x)
+
+var x = 0;
+if (true) {
+  let x = 12;
+  console.log(x);
+}
+console.log(x);
+
+// function greetUser () {
+//   let name = 'Hitsu'
+//   return function SayHello () {
+//     console.log(`Hello ${name}!`)
+//   }
+// }
+// greetUser()()
